8ee34e34523e7b844ddd746c1d6ef680a7a1ebe3
Sebastian <sebassuarez103@gmail.com>
2025-09-19 11:54:51 -0500

    Ciclo de vida de los objetos.

---
+++ src/streaming/play/MainStackHeap.java
@@ -0,0 +1,17 @@
+package streaming.play;
+
+import streaming.play.contenido.Pelicula;
+
+public class MainStackHeap {
+    public static void main(String[] args) {
+        Pelicula reyLeon = new Pelicula("El Rey León", 135, "Animada");
+        Pelicula harryPotter = new Pelicula("Harry Potter", 200, "Fantasía");
+
+        reyLeon = harryPotter;
+
+        reyLeon.titulo = "El Hobbit";
+
+        System.out.println("reyLeon: " + reyLeon.titulo);
+        System.out.println("harryPotter: " + harryPotter.titulo);
+    }
+}
