37c4991804978dd0a67f49edb82f07f98b4d17a3
Sebastian <sebassuarez103@gmail.com>
2025-09-19 11:31:40 -0500

    Constantes

--- src/streaming/play/Main.java
+++ src/streaming/play/Main.java
@@ -9,8 +9,11 @@ import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
+    public static final String NOMBRE_PLATAFORMA = "STREAMING PLAY 🍿";
+    public static final String VERSION = "1.0.0";
+
     public static void main(String[] args) {
-        System.out.println("streaming PLAY 🍿");
+        System.out.println(NOMBRE_PLATAFORMA + " v" + VERSION);
 
         String nombre = ScannerUtils.capturarTexto("Nombre del contenido");
         String genero = ScannerUtils.capturarTexto("Genero del contenido");
