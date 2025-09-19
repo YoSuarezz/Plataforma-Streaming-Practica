3caaa0eda9d58cdbc169d0398b8741c381b9fa3b
Sebastian <sebassuarez103@gmail.com>
2025-09-19 12:30:45 -0500

    Listas en Java.

--- src/streaming/play/Main.java
+++ src/streaming/play/Main.java
@@ -1,18 +1,16 @@
 package streaming.play;
 
 import streaming.play.contenido.Pelicula;
+import streaming.play.plataforma.Plataforma;
 import streaming.play.plataforma.Usuario;
 import streaming.play.util.ScannerUtils;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Scanner;
 
 public class Main {
     public static final String NOMBRE_PLATAFORMA = "STREAMING PLAY 🍿";
     public static final String VERSION = "1.0.0";
 
     public static void main(String[] args) {
+        Plataforma plataforma = new Plataforma(NOMBRE_PLATAFORMA);
         System.out.println(NOMBRE_PLATAFORMA + " v" + VERSION);
 
         String nombre = ScannerUtils.capturarTexto("Nombre del contenido");
@@ -21,8 +19,14 @@ public class Main {
         double calificacion = ScannerUtils.capturarDecimal("Calificacion del contenido");
 
         Pelicula pelicula = new Pelicula(nombre, duracion, genero, calificacion);
+        Pelicula pelicula2 = new Pelicula("The Batman", 180, "Accion", 4.5);
 
-        System.out.println(pelicula.obtenerFichaTecnica());
+        plataforma.agregarContenido(pelicula);
+        plataforma.agregarContenido(pelicula2);
+        System.out.println("Numero de elementos en la plataforma: " + plataforma.getContenido().size());
+        plataforma.eliminarContenido(pelicula2);
+
+        plataforma.mostrarContenido();
 
         Usuario usuario = new Usuario("Juan", "juan@platzi.com");
         usuario.ver(pelicula);
---
+++ src/streaming/play/plataforma/Plataforma.java
@@ -0,0 +1,39 @@
+package streaming.play.plataforma;
+
+import streaming.play.contenido.Pelicula;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Plataforma {
+    private String nombre;
+    private List<Pelicula> contenido;
+
+    public Plataforma(String nombre) {
+        this.nombre = nombre;
+        this.contenido = new ArrayList<>();
+    }
+
+    public void agregarContenido(Pelicula pelicula) {
+        this.contenido.add(pelicula);
+    }
+
+    public void mostrarContenido() {
+        for (Pelicula pelicula : contenido) {
+            System.out.println(pelicula.getTitulo());
+        }
+    }
+
+    public void eliminarContenido(Pelicula pelicula) {
+        this.contenido.remove(pelicula);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Pelicula> getContenido() {
+        return contenido;
+    }
+
+}
